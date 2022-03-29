0d9e4f956f6f5164b66abb4442479eaa39597c66
Mohamed Amine Chatteli <amine.chatteli01@gmail.com>
2022-03-29 21:31:46 +0100

    pricing plan component styles

--- src/components/styles/PricingSection.styled.js
+++ src/components/styles/PricingSection.styled.js
@@ -1,11 +1,22 @@
 import styled from "styled-components";
 
-export const StyledPricingSection=styled.div`
-    div{
-        width:60%;
-        margin:auto;
-    }
+export const StyledPricingSection = styled.div`
+    background-color:red;
+    display:flex;
+    align-items:center;
+    justify-content:center;
+    gap:3.12rem;
+    
 `
-export const StyledPricePlan=styled.div`
+export const StyledPricingHedear = styled.div`
+    width:40%;
+    margin:auto;
+    text-align:center;
+`
+export const StyledPricePlan = styled.div`
+    width:22.92%;
+    min-height:47.5rem;
+    background-color:white;
 
+   
 `
